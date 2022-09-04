551aed1bdf070a1652d791a406c7e5e3a53dea12
yongnianliu <lyn0328@qq.com>
2022-09-04 21:49:13 +0800

    Exercises:颠倒三角形

--- src/1.getting_started/3.3.shaders_class/shaders_class.cpp
+++ src/1.getting_started/3.3.shaders_class/shaders_class.cpp
@@ -51,11 +51,18 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
+    // float vertices[] = {
+    //     // positions         // colors
+    //      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+    //     -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+    //      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+    // };
+    // 颠倒三角形
     float vertices[] = {
         // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+         0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
     };
 
     unsigned int VBO, VAO;
